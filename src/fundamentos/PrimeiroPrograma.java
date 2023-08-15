b9b6880a18ec34aee494d1c5c66a8ac2a6e65629
Fernando Braz da Silveira <112355109+fernandobraz88@users.noreply.github.com>
2023-08-15 15:42:21 -0300

    Up

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -1,4 +1,23 @@
 package fundamentos;
 
-public class PrimeiroPrograma {
+public class PrimeiroPrograma
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Primeiro Programa");
+    }
 }
+
+/*PSVM - É uma assinatura de método especial que serve como ponto de entrada para a execução de um programa java
+
+PUBLIC: acessível de qualquer lugar do programa
+
+STATIC: indica que o método é inerente à classe, não a instancias. Ou seja, não é necessaria a criação de um objeto
+para chamar o metodo main
+
+VOID: indica que o metodo main não retorna nenhum valor para ser usado posteriormente
+
+MAIN: nome do metodo de entrada da execução do programa
+
+String[] args: parametros que o metodo main aceita, utilizado quando se executa um programa java pelas linhas de comando
+*/
