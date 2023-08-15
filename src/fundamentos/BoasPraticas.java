60c900ec801dc4436e86b55dfa992516e30ed1bc
Fernando Braz da Silveira <112355109+fernandobraz88@users.noreply.github.com>
2023-08-15 16:28:49 -0300

    up

---
+++ src/fundamentos/BoasPraticas.java
@@ -0,0 +1,29 @@
+package fundamentos;
+
+public class BoasPraticas
+{
+    public static void main(String[] args)
+    {
+
+        /* Comentários:
+         * Nas boas praticas de progamação é interessante deixar comentarios relevantes para que facilitem a leitura e
+         * compreensão do codigo por outros devs.
+         * Os comentários podem ser de unica linha (escritos após um //) ou de multiplas linhas (escritos entre um /* e */
+
+        /* Nomes Descritivos:
+         * Dê nomes significativos às variáveis, métodos, classes e pacotes para tornar o código autoexplicativo.*/
+
+        /* Convenções de Nomenclatura:
+         * Siga as convenções de nomenclatura Java
+         * CamelCase para classes (começando com letra maiúscula),
+         * camelCase para métodos e variáveis (começando com letra minúscula).
+         * palavras em letras maiúsculas para CONSTANTES.
+         * palavras em letras minusculas para packages */
+
+        /* Organização de Pacotes:
+        * Organize suas classes em pacotes para manter uma estrutura lógica.
+        * Use nomes de pacotes em minúsculas e significativos para evitar conflitos.*/
+
+
+    }
+}
