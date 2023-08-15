edc937bb2df11a72a21d31405efda4d28091400c
Fernando Braz da Silveira <112355109+fernandobraz88@users.noreply.github.com>
2023-08-15 18:28:40 -0300

    up

---
+++ src/fundamentos/A004TiposPrimitivos.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class A004TiposPrimitivos {
+    //4 numericos e 2 não numericos
+    /*Numericos:
+    Byte:
+    Short:
+    int:
+    long:
+     */
+    /*Não-Numericos:
+
+     */
+}
