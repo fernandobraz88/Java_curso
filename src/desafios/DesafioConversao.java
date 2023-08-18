29b600170d5969f1f17fa13fcf0361ba078cf6da
Fernando Braz da Silveira <112355109+fernandobraz88@users.noreply.github.com>
2023-08-18 13:00:18 -0300

    up

--- src/desafios/DesafioConversao.java
+++ src/desafios/DesafioConversao.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 
 public class DesafioConversao {
 
+    //Desfio: receber 3 valores de salario como entrada (em string) e converte-los para double para calcular uma media.
+    //Caso o usuário digite virgulas como ponto flutuante, esse tambem deverá ser aceito.
+
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         System.out.println("Digite o salario 1: ");
-        String salario1 = input.nextLine();
+        String salario1 = input.nextLine().replace(",",".");
         System.out.println("Digite o salario 2: ");
-        String salario2 = input.nextLine();
+        String salario2 = input.nextLine().replace(",",".");
         System.out.println("Digite o salario 3: ");
-        String salario3 = input.nextLine();
+        String salario3 = input.nextLine().replace(",",".");
 
 
         //usando o metodo parseDouble() da classe Wrapper Double
