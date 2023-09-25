779dea20742747b249ca18a403e88ff773b2e646
Fernando Braz da Silveira <112355109+fernandobraz88@users.noreply.github.com>
2023-09-25 16:08:59 -0300

    update 25/09

---
+++ src/orientacaoObjetos/Exercicio03/AreaCirc.java
@@ -0,0 +1,17 @@
+package orientacaoObjetos.Exercicio03;
+
+//testando atributos estaticos
+public class AreaCirc {
+
+    double raio;
+    static double pi = 3.14; //o valor definido no atributo estático será compartilhado por todas as instancas da classe
+
+    //construtor:
+    public AreaCirc(double raio){
+        this.raio = raio;
+    }
+
+    public double area(){
+        return pi*raio*raio;
+    }
+}
---
+++ src/orientacaoObjetos/Exercicio03/AreaCircTeste.java
@@ -0,0 +1,10 @@
+package orientacaoObjetos.Exercicio03;
+
+public class AreaCircTeste {
+
+    public static void main(String[] args) {
+    AreaCirc c1 = new AreaCirc(2.4);
+
+        System.out.println(c1.area());
+    }
+}
