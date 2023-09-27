9c49162c5a9bafa7f2de5f737a41fae138c88961
Fernando Braz da Silveira <112355109+fernandobraz88@users.noreply.github.com>
2023-09-27 16:31:35 -0300

    Update

---
+++ src/arraysColections/A002Matrizes.java
@@ -0,0 +1,30 @@
+package arraysColections;
+
+import java.util.Arrays;
+
+public class A002Matrizes {
+
+    //As matrizes são Estruturas de Dados capazes de armazenar na forma de tabelas (linhas e colunas)
+    //No java uma matriz trata-se de um Array de arrays.
+    //Cada elemento de uma matriz pode ser acessado pelo indice da linha e o da coluna.
+
+    public static void main(String[] args) {
+        //Construção de uma matriz: declarar primeiro o array extreno depois o interno
+
+        int [][] matriz = new int [2][3]; // Uma matriz de 2 linhas e 3 colunas
+
+        matriz [0][0] = 1;
+        matriz [0][1] = 2;
+        matriz [0][2] = 3;
+        matriz [1][0] = 4;
+        matriz [1][1] = 5;
+        matriz [1][2] = 6;
+
+        System.out.println(matriz[1][2]);
+
+        //ou declarar de forma explicita
+        int [][] matriz2 = {{7,8,9},{10,11,12},{13,14,15}};
+
+        System.out.println(matriz2[1][0]);
+    }
+}
--- src/fundamentos/A999Atipicos.java
+++ src/fundamentos/A999Atipicos.java
@@ -1,5 +1,8 @@
 package fundamentos;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class A999Atipicos {
 
     //nextInt() e nextDouble():
@@ -8,4 +11,11 @@ public class A999Atipicos {
     * Sendo assim o proximo nextLine() lerá esse /n como entrada.
     * Para resolver esse problema, precisamos "pegar" o "\n" que o nextInt()/nextDouble() não lê.
     * Isso pode ser feito colocando um comando nextLine() depois do nextInt()/nextDouble()*/
+
+    //nextDouble: useLocale => faz com que o scaner leia . no lugar da virgula em imputs
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double input = entrada.useLocale(Locale.US).nextDouble();
+    }
+
 }
